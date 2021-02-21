a933161a828c26e0dd40dda8b08b09b98cc3db2a
Duncan Alexander <hypo@hypothete.com>
2021-02-20 18:38:26 -0800

    getting effects, movement is weird

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import * as THREE from './node_modules/three/build/three.module.js';
+
 import textureList from './textures.js';
 
 const scene = new THREE.Scene();
@@ -10,7 +11,19 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(width, height);
 document.body.appendChild(renderer.domElement);
 camera.rotation.y = Math.PI;
+camera.position.z = -3;
 scene.add(camera);
+
+let fieldTexture;
+let plane, planeMat;
+let focusingPlane = 1;
+const virtualCam = new THREE.Vector3(0,0,-1);
+const cameraData = [];
+const camsX = 17;
+const camsY = 17;
+const resX = 1024;
+const resY = 1024;
+const camInterval = 0.08; // cm hardcoded for now
 
 window.addEventListener('resize', () => {
   width = window.innerWidth;
@@ -21,15 +34,53 @@ window.addEventListener('resize', () => {
   renderer.render(scene, camera);
 });
 
-let fieldTexture;
-let plane;
+window.addEventListener('wheel', (e) => {
+  focusingPlane += e.deltaY / (Math.abs(e.deltaY) * 10);
+  planeMat.uniforms.focusingPlane.value = focusingPlane;
+});
+
+window.addEventListener('keydown', (e) => {
+  const dMove = 0.01;
+  switch(e.key) {
+    case 'ArrowLeft':
+      virtualCam.x -= dMove;
+      break;
+    case 'ArrowRight':
+      virtualCam.x += dMove;
+      break;
+    case 'ArrowUp':
+      virtualCam.y += dMove;
+      break;
+    case 'ArrowDown':
+      virtualCam.y -= dMove;
+    default:
+  }
+});
 
 loadScene();
+
+function animate() {
+	requestAnimationFrame( animate );
+	renderer.render( scene, camera );
+
+}
 
 async function loadScene() {
   await loadField();
   loadPlane();
-  renderer.render(scene, camera);
+  animate();
+}
+
+function addCameraData(filename) {
+  // out_00_00_-230.697815_929.138916_.png
+  const dataStr = filename.split('_');
+  const datum = {
+    x: Number(dataStr[1]),
+    y: Number(dataStr[2]),
+    dx: Number(dataStr[3]),
+    dy: Number(dataStr[4]),
+  };
+  cameraData.push(datum);
 }
 
 function imgToRGBABuffer(img,w,h) {
@@ -49,8 +100,9 @@ function imgToRGBABuffer(img,w,h) {
 async function loadField() {
   const textureLoader = new THREE.TextureLoader();
   const bufferTx = await Promise.all(textureList.map(async filename => {
+    addCameraData(filename);
     const loadedTx = await textureLoader.loadAsync(`./data/${filename}`);
-    return imgToRGBABuffer(loadedTx.image, 1024, 1024);
+    return imgToRGBABuffer(loadedTx.image, resX, resY);
   }));
   const totalBytes = bufferTx.reduce((acc, buf) => acc + buf.byteLength, 0);
   const allBuffer = new Uint8Array(totalBytes);
@@ -59,22 +111,24 @@ async function loadField() {
     allBuffer.set(buf, offset);
     offset += buf.byteLength;
   });
-  fieldTexture = new THREE.DataTexture2DArray(allBuffer, 1024, 1024, 17 * 17);
+  fieldTexture = new THREE.DataTexture2DArray(allBuffer, resX, resY, camsX * camsY);
   console.log('Loaded field data.');
 }
 
 function loadPlane() {
-  const planeGeo = new THREE.PlaneGeometry(1,1,1,1);
-  const planeMat = new THREE.ShaderMaterial({
+  const planeGeo = new THREE.PlaneGeometry(camsX * camInterval, camsY * camInterval, camsX, camsY);
+  planeMat = new THREE.ShaderMaterial({
     uniforms: {
       field: { value: fieldTexture },
+      virtualCam: new THREE.Uniform(virtualCam),
+      focusingPlane: { value: focusingPlane },
+      camArraySize: new THREE.Uniform(new THREE.Vector2(camsX, camsY))
     },
     vertexShader: document.querySelector('script[type="x-shader/x-vertex"]').textContent,
     fragmentShader: document.querySelector('script[type="x-shader/x-fragment"]').textContent,
   });
   plane = new THREE.Mesh(planeGeo, planeMat);
   scene.add(plane);
-  plane.position.z = 1.2;
   plane.rotation.y = Math.PI;
   console.log('Loaded plane.');
 }
